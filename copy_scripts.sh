c1f545bb32c4dd8be1c345b0cd1ef2b55563bfd3
aryan <admin@reticentadmin.com>
2024-12-20 23:10:41 +0000

    feat: add shell scripts from other git repositories

--- copy_scripts.sh
+++ copy_scripts.sh
@@ -7,16 +7,28 @@
 # Copyright (c) 2024 Aryan
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Version: 1.0.0
+# Version: 2.0.0
 
-mkdir -p "./scripts/"
+mkdir -p "./system-scripts/"
+mkdir -p "./other-scripts/"
 
-# sbin scripts
+# system-scripts
+
+## sbin scripts
 cp "/usr/local/sbin/firewall" "./scripts/firewall.sh"
 cp "/usr/local/sbin/fix_ownership" "./scripts/fix_ownership.sh"
 cp "/usr/local/sbin/remove_chromium_deps" "./scripts/remove_chromium_deps.sh"
 
-# bin scripts
+## bin scripts
 cp "/usr/local/bin/fix_perms" "./scripts/fix_perms.sh"
 cp "/usr/local/bin/set_wallpaper" "./scripts/set_wallpaper.sh"
 cp "/usr/local/bin/shot" "./scripts/shot.sh"
+
+# Other scripts
+
+## etc-configs
+cp "../../linux/etc-configs/copy_etc_configs.sh" "./other-scripts/"
+
+## kernel-configs
+cp "../../linux/kernel-configs/copy_config.sh" "./other-scripts/"
+cp "../../linux/kernel-configs/replace_cmdline.sh" "./other-scripts/"
